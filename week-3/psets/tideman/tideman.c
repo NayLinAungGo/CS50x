53340a183ab9c310978e1ff547036e842ebb79ab
nay <naylinaunggo10@gmail.com>
2026-02-02 23:44:39 +0630

    Implement: half of the functions for week-3 pset tideman
    Implement vote(), record_preferences(), add_pairs(), sort_pairs() and print_preferences() for debugging

--- week-3/psets/tideman/tideman.c
+++ week-3/psets/tideman/tideman.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max number of candidates
 #define MAX 9
@@ -98,28 +99,96 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
+    // rank is current from Rank 1:
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i], name) == 0)
+        {
+            ranks[rank] = i;
+            return true;
+        }
+    }
     return false;
 }
 
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // ranks[i] over ranks[j...n] where j = (i + 1)
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            //printf("+1 to candidate %i over candidate %i\n", ranks[i], ranks[j]);
+            preferences[ranks[i]][ranks[j]]++;
+            //printf("It is now: %i\n", preferences[ranks[i]][ranks[j]]);
+        }
+    }
     return;
 }
 
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
+    // Check through each unique pair of candidates
+    for (int i = 0; i < candidate_count; i++)
+        for (int j = i + 1; j < candidate_count; j++)
+        {
+            printf("Checking candidates %i \& %i\n", i, j);
+            if (preferences[i][j] > preferences[j][i])
+            {
+                printf("Candidate %i is preferred over %i\n", i, j);
+
+                pair_count++;
+                pairs[pair_count - 1].winner = i;
+                pairs[pair_count - 1].loser = j;
+            }
+            else if (preferences[i][j] < preferences[j][i])
+            {
+                printf("Candidate %i is preferred over %i\n", j, i);
+
+                pair_count++;
+                pairs[pair_count - 1].winner = j;
+                pairs[pair_count - 1].loser = i;
+            }
+        }
     return;
+}
+
+void print_preferences(void)
+{
+    printf("\nPreferences are\n");
+    for (int i = 0; i < pair_count; i++)
+    {
+        printf("%i\n", preferences[pairs[i].winner][pairs[i].loser]);
+    }
+    printf("\n");
 }
 
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    // TODO
+    print_preferences();
+    // Using bubble sort
+    bool swapped;
+    for (int i = 0; i < pair_count - 1; i++)
+    {
+        swapped = false;
+        for (int j = 0; j < pair_count - i - 1; j++)
+        {
+            if (preferences[pairs[j].winner][pairs[j].loser] <
+                preferences[pairs[j + 1].winner][pairs[j + 1].loser])
+            {
+                pair temp_pair = pairs[j];
+                pairs[j] = pairs[j + 1];
+                pairs[j + 1] = temp_pair;
+                printf("swapped pairs[%i] with pairs[%i]\n", j, j + 1);
+                swapped = true;
+            }
+        }
+        if (swapped == false)
+            break;
+    }
     return;
 }
 
