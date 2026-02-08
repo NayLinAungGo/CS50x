a6a24f9cd8adc4369261dcd093f41a7d23bd6658
nay <naylinaunggo10@gmail.com>
2026-02-08 16:28:52 +0630

    Implement: grayscale function

--- week-4/psets/filter-less/helpers.c
+++ week-4/psets/filter-less/helpers.c
@@ -1,8 +1,19 @@
+#include <math.h>
+
 #include "helpers.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Initialize pixel pointer at the first one
+    RGBTRIPLE *pixel = &image[0][0];
+    // Loop through each pixel
+    for (int i = 0; i < height * width; i++)
+        // Assign gray-scale converted value to all the color channels
+        pixel[i].rgbtRed = pixel[i].rgbtGreen = pixel[i].rgbtBlue =
+            // Luma-corrected grayscale formula used by GIMP
+            round(pixel[i].rgbtRed * 0.3 + pixel[i].rgbtGreen * 0.59 + pixel[i].rgbtBlue * 0.11);
+
     return;
 }
 
